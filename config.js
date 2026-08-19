66bbd9996ae4f1d3ae5d5e30521290235d68786c
adelkaa-gif <adelkauraeva@gmail.com>
2026-08-19 23:21:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://mfyuscxmtfoaqoxpavfb.supabase.co";
+const SUPABASE_URL = "https://mfyuscxmtfoaqoxpavfb.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "sb_publishable_1tAxC9538zJEOyDjOC9I7w_g41ZsCie";
