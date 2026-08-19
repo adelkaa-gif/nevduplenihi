84cac328b2cea5fa2a5cbafc2f2e65d5c7815e74
adelkaa-gif <adelkauraeva@gmail.com>
2026-08-19 10:10:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Заполни эти 2 значения после создания проекта Supabase.
 const SUPABASE_URL = "https://mfyuscxmtfoaqoxpavfb.supabase.co";
-const SUPABASE_ANON_KEY = "mfyuscxmtfoaqoxpavfb";
+const SUPABASE_ANON_KEY = "sb_publishable_1tAxC9538zJEOyDjOC9I7w_g41ZsCie";
